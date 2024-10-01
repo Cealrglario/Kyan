c35fed5bc6d66a723b6ab7604b35959c0fbc6ffa
Carl Elgario <kirstencarl.elgario@ucalgary.ca>
2024-10-01 13:57:25 -0600

    haircut

---
+++ Haircut.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int choice;
+    
+    printf("What's your choice bud 3");
+    int check = scanf("%d", &choice);
+
+    printf("Your choice is %d", choice);
+    printf("\nZesty ahh");
+    
+    return 0;
+}
